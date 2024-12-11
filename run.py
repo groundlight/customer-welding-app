7ff084da9d66929abfeea6f99a8d718871436fc3
Harry Tung <honeytung@gmail.com>
2024-12-11 11:05:13 -0800

    Added routing

--- run.py
+++ run.py
@@ -11,5 +11,10 @@ GLHUB_APP_PORT = int(os.getenv("GLHUB_APP_PORT", "8000"))
 if __name__ == "__main__":
     app = create_app()
     
+    # Adjust application proxy when running behind a reverse proxy (balenaCloud)
+    # Check if we are running through balenaCloud (It is running on balena if the environment variable BALENA is set)
+    if os.getenv("BALENA"):
+        app.config["APPLICATION_ROOT"] = "/apps/app_1"
+    
     logger.info(f"Running weld app on port {GLHUB_APP_PORT}")
     app.run(host="0.0.0.0", port=GLHUB_APP_PORT)
