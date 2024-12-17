7c0f1b8306bced48e16bbb58be4693cb5cd7fc9b
Harry Tung <honeytung@gmail.com>
2024-12-17 15:17:33 -0800

    Added unit test template and workflow

---
+++ tests/test_backend.py
@@ -0,0 +1,13 @@
+import unittest
+
+from weld import backend
+
+
+class TestSaturatingCounter(unittest.TestCase):
+    def test_update(self):
+        """Test the update method of the SaturatingCounter class."""
+
+        counter = backend.SaturatingCounter()
+
+        assert counter.update(False) == "NO"
+        assert counter.update(True) == "YES"
